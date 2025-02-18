3d839faa6664f3b6dbeb59159fec32c50fb01ff2
Bruno Reis <134024017+dev-brunoreis@users.noreply.github.com>
2025-02-18 11:34:14 -0300

    adjust whatsapp link in mobile and desktop

--- components/page/header/buttons.vue
+++ components/page/header/buttons.vue
@@ -13,9 +13,8 @@ const toggleColorMode = () => {
 
 onMounted(() => {
     const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent)
-    whatsappLink.value = isMobile
-        ? 'whatsapp://send?phone=5543988488170'
-        : 'https://web.whatsapp.com/send?phone=5543988488170'
+    const message = encodeURIComponent('Oi, quero fazer um orçamento!')
+    whatsappLink.value = `https://${isMobile ? 'api' : 'web'}.whatsapp.com/send?phone=5543988488170&text=${message}`
 })
 </script>
 
